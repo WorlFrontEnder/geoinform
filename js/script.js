9f7b8ab918ff496aa84b64bb37527f67ca4f0b19
WorlFrontEnder <Cyberverstalshik@yandex.ru>
2018-04-18 14:11:38 +0400

    Инициализация скриптов javascript

---
+++ js/script.js
@@ -0,0 +1,25 @@
+(function($){
+			$(window).load(function(){
+				
+				$(".demo-x").mCustomScrollbar({
+					axis:"x",
+					advanced:{autoExpandHorizontalScroll:true}
+				});
+				 
+             
+
+			});
+		})(jQuery);
+
+
+$(document).ready(function() {
+
+ $('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1
+});
+
+ 
+
+});
